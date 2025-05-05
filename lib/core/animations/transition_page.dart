c9988e19ce15931986549635a13624ed4d71d049
vonjy-2101 <ralvonjy@gmail.com>
2025-05-05 13:34:00 +0300

    remove space

--- lib/core/animations/transition_page.dart
+++ lib/core/animations/transition_page.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class CustomTransitionPageGo<T> extends CustomTransitionPage<T>{
-
-    Widget pageScreen;
-    CustomTransitionPageGo({required this.pageScreen}) : super(
+class CustomTransitionPageGo<T> extends CustomTransitionPage<T> {
+  Widget pageScreen;
+  CustomTransitionPageGo({required this.pageScreen})
+    : super(
         child: pageScreen,
         transitionDuration: const Duration(milliseconds: 150),
         reverseTransitionDuration: const Duration(milliseconds: 150),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            final isReverse = animation.status == AnimationStatus.reverse;
-            final begin = isReverse
-                ? const Offset(-1.0, 0.0)
-                : const Offset(1.0, 0.0);
-            const end = Offset.zero;
-            final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.fastOutSlowIn));
+          final isReverse = animation.status == AnimationStatus.reverse;
+          final begin = isReverse ? const Offset(-1.0, 0.0) : const Offset(1.0, 0.0);
+          const end = Offset.zero;
+          final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.fastOutSlowIn));
 
-            return SlideTransition(
-                position: animation.drive(tween),
-                child: child,
-            );
+          return SlideTransition(position: animation.drive(tween), child: child);
         },
-    );
-
-    
-    
+      );
 }
